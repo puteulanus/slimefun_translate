f3ef7c52b7064eaad872414956f19e09ee521033
Puteulanus <mj@puteulanus.com>
2016-06-29 16:07:37 +0800

    fix bug

--- build-script/build.sh
+++ build-script/build.sh
@@ -8,8 +8,8 @@ mkdir -p /usr/www/default/public_html/${plugin_name}
 while read line
 do
 	rm -f tmp new.jar
-	version=$(echo ${line} | awk -F ':' '{print $1}')
-	file_url=$(echo ${line} | awk -F ':' '{print $2}')
+	version=$(echo ${line} | awk -F ' ' '{print $1}')
+	file_url=$(echo ${line} | awk -F ' ' '{print $2}')
 	wget -O org.jar ${file_url}
 	java -jar pf.jar do -d {dic_name} org.jar
 	rm -rf org.jar
