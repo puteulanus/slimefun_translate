4192de1470bab28696180003b7e18a3168b584b5
Puteulanus <mj@puteulanus.com>
2016-06-29 18:12:59 +0800

    fix bug

--- build-script/build.sh
+++ build-script/build.sh
@@ -3,7 +3,6 @@
 plugin_name=$1
 dic_name=$2
 
-cd ${plugin_name}
 mkdir -p /usr/www/default/public_html/${plugin_name}
 while read line
 do
@@ -11,7 +10,7 @@ do
 	version=$(echo ${line} | awk -F ' ' '{print $1}')
 	file_url=$(echo ${line} | awk -F ' ' '{print $2}')
 	wget -O org.jar ${file_url}
-	java -jar pf.jar do -d {dic_name} org.jar
+	java -jar pf.jar do -d ${plugin_name}/${dic_name} org.jar
 	rm -rf org.jar
 	mv new.jar /usr/www/default/public_html/${plugin_name}/${plugin_name}_${version}_${dic_name}.jar
 done < version.txt
